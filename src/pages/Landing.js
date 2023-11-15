4349a8b29faebd9244bcc5b8e71e88e42ecb8699
David <dagrove7@gmail.com>
2023-11-15 16:12:51 -0500

    added react router and new pages

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,11 +1,14 @@
-import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Link } from 'react-router-dom';
+
+import { Logo } from '../components';
 
 const Landing = () => {
     return (
-        <main>
+        <Wrapper>
             <nav>
-                <img src={logo} alt="jobster logo" className="logo" />
+                <Logo />
             </nav>
             <div className="container page">
                 {/* info */}
@@ -18,11 +21,13 @@ const Landing = () => {
                         elit. Culpa atque ut impedit nam nostrum vitae aliquid
                         vel rerum adipisci earum.
                     </p>
-                    <button className="btn btn-hero">Login/Register</button>
+                    <Link to="/register" className="btn btn-hero">
+                        Login/Register
+                    </Link>
                 </div>
                 <img src={main} alt="job hunt" className="img main-img" />
             </div>
-        </main>
+        </Wrapper>
     );
 };
 
