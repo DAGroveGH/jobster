11fa64eed7000fce94cb50a1d0fdcda048214c36
David <dagrove7@gmail.com>
2023-12-09 19:08:58 -0500

    set up jobs structure and slice

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,20 @@ import Navbar from './Navbar';
 import BigSidebar from './BigSidebar';
 import SmallSidebar from './SmallSidebar';
 import FormRowSelect from './FormRowSelect';
+import SearchContainer from './SearchContainer';
+import JobsContainer from './JobsContainer';
+import Job from './Job';
+import Loading from './Loading';
 
-export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar, FormRowSelect };
+export {
+    Logo,
+    FormRow,
+    Navbar,
+    BigSidebar,
+    SmallSidebar,
+    FormRowSelect,
+    SearchContainer,
+    JobsContainer,
+    Job,
+    Loading,
+};
